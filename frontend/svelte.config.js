1bc15c0995c002504620d382e2be332ea1779d41
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-05-08 14:50:59 +0200

    Update svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,5 +5,10 @@ export default {
   kit: {
     adapter: adapter({ fallback: 'index.html' }),   // SPA mode
     prerender: { entries: [] }                      // skip crawling
+  },
+
+  proxy: {
+    '/analyze':       'http://localhost:5000',
+    '/analyze-media': 'http://localhost:5000'
   }
 };
