d133ac836fe79a548d0091055e2b8143e0918de7
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-05-08 14:18:33 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-import cors from 'cors';
 import path from "path";
 import { fileURLToPath } from "url";  
 
